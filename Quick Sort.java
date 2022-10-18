4084b2b9429b77fb30d47519f2007856f50812e9
Yuvraj Singh <63182105+yuvithakur007@users.noreply.github.com>
2022-10-19 00:26:22 +0530

    Solved Quick Sort issue #7
    Implement Quick Sort using recursion in JAVA.

---
+++ Quick Sort.java
@@ -0,0 +1,35 @@
+import java.util.Arrays;
+public class QuickSort {
+    public static void main(String[] args) {
+        int[] arr = {75,5,14,10,56,49,66};
+       sort(arr, 0, arr.length - 1);
+       System.out.println(Arrays.toString(arr));
+    }
+
+    static void sort(int[] nums, int low, int hi) {
+        if (low >= hi) {
+            return;
+        }
+        int s = low;
+        int e = hi;
+        int m = s + (e - s) / 2;
+        int pivot = nums[m];
+        while (s <= e) {
+            while (nums[s] < pivot) {
+                s++;
+            }
+            while (nums[e] > pivot) {
+                e--;
+            }
+            if (s <= e) {
+                int temp = nums[s];
+                nums[s] = nums[e];
+                nums[e] = temp;
+                s++;
+                e--;
+            }
+        }
+        sort(nums, low, e);
+        sort(nums, s, hi);
+    }
+}
